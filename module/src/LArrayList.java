28ee7888e4ce114b8b4bbb2a031c662ca4a52105
Pucan <martin.pucan@pilulka.cz>
2020-04-16 00:46:32 +0200

    add classes

---
+++ module/src/LArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LArrayList {
+    private ArrayList<Integer> cisla;
+    private Random random;
+
+    public LArrayList() {
+        random = new Random();
+        cisla = new ArrayList<Integer>();
+    }
+
+}
